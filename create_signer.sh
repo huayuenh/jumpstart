a4d082004c3794c36a5c4f07023551cc95af13be
Hua Yuen <huayuenh@ie.ibm.com>
2019-09-09 13:59:46 +0100

    create key if not found

--- create_signer.sh
+++ create_signer.sh
@@ -5,14 +5,13 @@ if [ -z "$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE" ]; then
     export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$(openssl rand -base64 16)
 fi
 
-echo "Create  $DEVOPS_SIGNER singer key"
 export DOCKER_CONTENT_TRUST=1
-docker trust key generate "$DEVOPS_SIGNER"
-echo "Restoring keys from $VAULT_INSTANCE"
+
 #set Vault access
 VAULT_DATA=$(buildVaultAccessDetailsJSON "$VAULT_INSTANCE" "$IBMCLOUD_TARGET_REGION" "$IBMCLOUD_TARGET_RESOURCE_GROUP")
 
 #retrieve existing keys from Vault
+echo "Checking Key Protect Vault for keys"
 JSON_PRIV_DATA="$(readData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA")"
 JSON_PUB_DATA="$(readData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA")"
 
@@ -21,30 +20,32 @@ echo "***************************"
 echo "$JSON_PRIV_DATA"
 echo "***************"
 EXISTING_KEY="$(getJSONValue "$DEVOPS_SIGNER" "$JSON_PRIV_DATA")"
-echo "search for $DEVOPS_SIGNER"
 echo "$EXISTING_KEY"
 if [[ "$EXISTING_KEY" == "null" || -z "$EXISTING_KEY" ]]; then
-    echo "key not found"
+    echo "Key for $DEVOPS_SIGNER not found."
+    echo "Create  $DEVOPS_SIGNER singer key"
+    docker trust key generate "$DEVOPS_SIGNER"
+    echo "Restoring keys from $VAULT_INSTANCE"
+    # add new keys to json
+    JSON_PRIV_DATA=$(addTrustFileToJSON "$DEVOPS_SIGNER" "$JSON_PRIV_DATA" "$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE")
+    base64PublicPem=$(base64TextEncode "./$DEVOPS_SIGNER.pub")
+    publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "name" "$DEVOPS_SIGNER.pub")
+    publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "value" "$base64PublicPem")
+    JSON_PUB_DATA=$(addJSONEntry "$JSON_PUB_DATA" "$DEVOPS_SIGNER" "$publicKeyEntry")
+
+    # delete old keys to allow for update
+    if [ "$JSON_PRIV_DATA" ]; then
+        deleteSecret "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA"
+        deleteSecret "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA"
+    fi
+
+    #save public/private key pairs to the vault
+    saveData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA" "$JSON_PRIV_DATA"
+    saveData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA" "$JSON_PUB_DATA"
 else
-    echo "key found"
-fi
-
-# add new keys to json
-JSON_PRIV_DATA=$(addTrustFileToJSON "$DEVOPS_SIGNER" "$JSON_PRIV_DATA" "$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE")
-base64PublicPem=$(base64TextEncode "./$DEVOPS_SIGNER.pub")
-publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "name" "$DEVOPS_SIGNER.pub")
-publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "value" "$base64PublicPem")
-JSON_PUB_DATA=$(addJSONEntry "$JSON_PUB_DATA" "$DEVOPS_SIGNER" "$publicKeyEntry")
-
-# delete old keys to allow for update
-if [ "$JSON_PRIV_DATA" ]; then
-    deleteSecret "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA"
-fi
-if [ "$JSON_PUB_DATA" ]; then
-    deleteSecret "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA"
+    echo "key for $DEVOPS_SIGNER already exists"
+    echo "No op"
 fi
 
 
-#save public/private key pairs to the vault
-saveData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA" "$JSON_PRIV_DATA"
-saveData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA" "$JSON_PUB_DATA"
+
