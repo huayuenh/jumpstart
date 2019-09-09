f0f75c18932f25a7f132f09158bb047079df2984
Hua Yuen <huayuenh@ie.ibm.com>
2019-09-09 19:28:07 +0100

    wrap delete

--- signatures.sh
+++ signatures.sh
@@ -230,19 +230,23 @@ function base64TextDecode {
 }
 
 function deleteSecret {
-    local vault_key=$1
+    echo "wrap delete"
+    local KEY=$1
     local VAULT_DATA=$2
     local VAULT_NAME=$(getJSONValue "name" "$VAULT_DATA")
     local VAULT_REGION=$(getJSONValue "region" "$VAULT_DATA")
     local VAULT_RESOURCE_GROUP=$(getJSONValue "resourcegroup" "$VAULT_DATA")
-    DELETE_SECRET_RESPONSE=$(
-        delete_secret \
-          "$VAULT_NAME" \
-          "$VAULT_REGION" \
-          "$VAULT_RESOURCE_GROUP" \
-          "$vault_key"
-      )
-      echo "DELETE_SECRET_RESPONSE=${DELETE_SECRET_RESPONSE}"
+
+    if [[ "$VAULT_NAME" && "$VAULT_REGION" && "$VAULT_RESOURCE_GROUP" && "$KEY" ]]; then
+        DELETE_SECRET_RESPONSE=$(
+            delete_secret \
+            "$VAULT_NAME" \
+            "$VAULT_REGION" \
+            "$VAULT_RESOURCE_GROUP" \
+            "$KEY"
+        )
+        echo "DELETE_SECRET_RESPONSE=${DELETE_SECRET_RESPONSE}"
+    fi
 }
 
 function deleteVault {
