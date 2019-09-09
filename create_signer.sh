df744d75cbe96552478efebc2c32c74c28f17610
Hua Yuen <huayuenh@ie.ibm.com>
2019-09-09 13:17:05 +0100

    test key check

--- create_signer.sh
+++ create_signer.sh
@@ -9,10 +9,28 @@ echo "Create  $DEVOPS_SIGNER singer key"
 export DOCKER_CONTENT_TRUST=1
 docker trust key generate "$DEVOPS_SIGNER"
 echo "Restoring keys from $VAULT_INSTANCE"
+#set Vault access
 VAULT_DATA=$(buildVaultAccessDetailsJSON "$VAULT_INSTANCE" "$IBMCLOUD_TARGET_REGION" "$IBMCLOUD_TARGET_RESOURCE_GROUP")
+
 #retrieve existing keys from Vault
 JSON_PRIV_DATA="$(readData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA")"
 JSON_PUB_DATA="$(readData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA")"
+
+echo "***************************"
+#check if entry exists
+EXISTING_KEY="$(getJSONValue "$DEVOPS_SIGNER" "$JSON_PRIV_DATA")"
+if [ "$EXISTING_KEY" ]; then
+    echo "key found"
+else
+    echo "key not found"
+fi
+
+# add new keys to json
+JSON_PRIV_DATA=$(addTrustFileToJSON "$DEVOPS_SIGNER" "$JSON_PRIV_DATA" "$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE")
+base64PublicPem=$(base64TextEncode "./$DEVOPS_SIGNER.pub")
+publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "name" "$DEVOPS_SIGNER.pub")
+publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "value" "$base64PublicPem")
+JSON_PUB_DATA=$(addJSONEntry "$JSON_PUB_DATA" "$DEVOPS_SIGNER" "$publicKeyEntry")
 
 # delete old keys to allow for update
 if [ "$JSON_PRIV_DATA" ]; then
@@ -22,12 +40,6 @@ if [ "$JSON_PUB_DATA" ]; then
     deleteSecret "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA"
 fi
 
-# add new keys to json
-JSON_PRIV_DATA=$(addTrustFileToJSON "$DEVOPS_SIGNER" "$JSON_PRIV_DATA" "$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE")
-base64PublicPem=$(base64TextEncode "./$DEVOPS_SIGNER.pub")
-publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "name" "$DEVOPS_SIGNER.pub")
-publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "value" "$base64PublicPem")
-JSON_PUB_DATA=$(addJSONEntry "$JSON_PUB_DATA" "$DEVOPS_SIGNER" "$publicKeyEntry")
 
 #save public/private key pairs to the vault
 saveData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA" "$JSON_PRIV_DATA"
