fb6f1f916addf4ab43dac0304d431899d60d744d
Hua Yuen <huayuenh@ie.ibm.com>
2019-09-30 08:59:56 +0100

    merge scripts

--- signatures.sh
+++ signatures.sh
@@ -267,3 +267,169 @@ function deleteVault {
       )
       echo "DELETE_VAULT_RESPONSE=${DELETE_VAULT_RESPONSE}"
 }
+
+function createSigner {
+
+    if [ -z "$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE" ]; then
+        export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$(openssl rand -base64 16)
+    fi
+
+    export DOCKER_CONTENT_TRUST=1
+
+    #set Vault access
+    VAULT_DATA=$(buildVaultAccessDetailsJSON "$VAULT_INSTANCE" "$IBMCLOUD_TARGET_REGION" "$IBMCLOUD_TARGET_RESOURCE_GROUP")
+
+    #retrieve existing keys from Vault
+    echo "Checking Key Protect Vault for keys"
+    JSON_PRIV_DATA="$(readData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA")"
+    JSON_PUB_DATA="$(readData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA")"
+    EXISTING_KEY="$(getJSONValue "$DEVOPS_SIGNER" "$JSON_PRIV_DATA")"
+    if [[ "$EXISTING_KEY" == "null" || -z "$EXISTING_KEY" ]]; then
+        echo "Key for $DEVOPS_SIGNER not found."
+        echo "Create  $DEVOPS_SIGNER singer key"
+        docker trust key generate "$DEVOPS_SIGNER"
+        # add new keys to json
+        JSON_PRIV_DATA=$(addTrustFileToJSON "$DEVOPS_SIGNER" "$JSON_PRIV_DATA" "$DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE")
+        base64PublicPem=$(base64TextEncode "./$DEVOPS_SIGNER.pub")
+        publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "name" "$DEVOPS_SIGNER.pub")
+        publicKeyEntry=$(addJSONEntry "$publicKeyEntry" "value" "$base64PublicPem")
+        JSON_PUB_DATA=$(addJSONEntry "$JSON_PUB_DATA" "$DEVOPS_SIGNER" "$publicKeyEntry")
+    
+    
+        # delete old keys to allow for update
+        if [ "$JSON_PRIV_DATA" ]; then
+            deleteSecret "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA"
+            deleteSecret "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA"
+        fi
+
+        #save public/private key pairs to the vault
+        saveData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA" "$JSON_PRIV_DATA"
+        saveData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA" "$JSON_PUB_DATA"
+    else
+        echo "key for $DEVOPS_SIGNER already exists"
+        echo "No op"
+    fi
+}
+
+function deleteSigner {
+    export DOCKER_CONTENT_TRUST=1
+
+    #set Vault access
+    VAULT_DATA=$(buildVaultAccessDetailsJSON "$VAULT_INSTANCE" "$IBMCLOUD_TARGET_REGION" "$IBMCLOUD_TARGET_RESOURCE_GROUP")
+
+    #retrieve existing keys from Vault
+    echo "Checking Key Protect Vault for keys"
+    JSON_PRIV_DATA="$(readData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA")"
+    JSON_PUB_DATA="$(readData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA")"
+    EXISTING_KEY="$(getJSONValue "$DEVOPS_SIGNER" "$JSON_PRIV_DATA")"
+
+    if [ "$EXISTING_KEY" ]; then
+        echo "Key for $DEVOPS_SIGNER  found."
+        echo "Removing  $DEVOPS_SIGNER singer key"
+        # add new keys to json
+        JSON_PRIV_DATA=$(removeJSONEntry "$JSON_PRIV_DATA" "$DEVOPS_SIGNER")
+        JSON_PUB_DATA=$(removeJSONEntry "$JSON_PUB_DATA" "$DEVOPS_SIGNER")
+        # delete old keys to allow for update
+        if [ "$JSON_PRIV_DATA" ]; then
+            deleteSecret "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA"
+            deleteSecret "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA"
+        fi
+
+        #save public/private key pairs to the vault
+        saveData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA" "$JSON_PRIV_DATA"
+        saveData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA" "$JSON_PUB_DATA"
+    else
+        echo "key for $DEVOPS_SIGNER already exists"
+        echo "No op"
+    fi
+}
+
+#remove the key from json
+function findSigner {
+    local SIGNER=$1
+    local IMAGE_TAG=$2
+    local GUN=$3
+    if [ -z "$REGISTRY_URL" ]; then
+    # Use the ibmcloud cr info to find the target registry url 
+    export REGISTRY_URL=$(ibmcloud cr info | grep -m1 -i '^Container Registry' | awk '{print $3;}')
+    fi
+
+    export GUN="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
+    export DOCKER_CONTENT_TRUST_SERVER=${DOCKER_CONTENT_TRUST_SERVER:-"https://$REGISTRY_URL:4443"}
+    echo "DOCKER_CONTENT_TRUST_SERVER=$DOCKER_CONTENT_TRUST_SERVER"
+    trustData=$(docker trust inspect "$GUN")
+    # Check if the Builder signature is present
+    if jq -e ".[] | .SignedTags[] | select(.SignedTag=\"$IMAGE_TAG\") | select (.Signers[] | contains(\"$SIGNER\"))" <<<"$trustData"; then
+        echo "$BUILD_SIGNER found"
+        echo "true"
+    else
+        echo "$BUILD_SIGNER not found"
+        echo "false"
+    fi
+}
+
+function findTrustData {
+    local GUN=$1
+    if [ -z "$REGISTRY_URL" ]; then
+    # Use the ibmcloud cr info to find the target registry url 
+    export REGISTRY_URL=$(ibmcloud cr info | grep -m1 -i '^Container Registry' | awk '{print $3;}')
+    fi
+
+    export GUN="$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
+    export DOCKER_CONTENT_TRUST_SERVER=${DOCKER_CONTENT_TRUST_SERVER:-"https://$REGISTRY_URL:4443"}
+    echo "DOCKER_CONTENT_TRUST_SERVER=$DOCKER_CONTENT_TRUST_SERVER"
+    trustData=$(docker trust inspect "$GUN")
+    result=$(jq -e ".[]" <<<"$trustData")
+    if [ "$result" ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+function revokeSigner {
+    VAULT_DATA=$(buildVaultAccessDetailsJSON "$VAULT_INSTANCE" "$IBMCLOUD_TARGET_REGION" "$IBMCLOUD_TARGET_RESOURCE_GROUP")
+    #write repo pem file to trust/private. Only repo key required to add delegate
+    export CURRENT_PATH=$(pwd)
+    JSON_REPO_DATA="$(readData "$REGISTRY_NAMESPACE.$IMAGE_NAME.repokeys" "$VAULT_DATA")"
+    repokey=$(getJSONValue "target" "$JSON_REPO_DATA")
+    writeFile "$repokey"
+
+    #retrieve public keys
+    PUBLIC_DATA="$(readData "$REGISTRY_NAMESPACE.pub" "$VAULT_DATA")"
+    publickey=$(getJSONValue "$DEVOPS_SIGNER" "$PUBLIC_DATA")
+    #write out/create the public key file to the system
+    writeFile "$publickey" "$CURRENT_PATH"
+    export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$(getJSONValue "passphrase" "$repokey")
+    #docker trust signer remove prompts for confirmation
+    echo y | docker trust signer remove "$DEVOPS_SIGNER" "$GUN"
+    docker trust inspect --pretty $GUN
+}
+
+function signImage {
+    #Requires
+    #VAULT_INSTANCE -name of vault
+    # $IBMCLOUD_TARGET_REGION -region hosting Key Protect Vault instance
+    # $IBMCLOUD_TARGET_RESOURCE_GROUP of the Key Protect Vault Instance
+    # REGISTRY_NAMESPACE namespace of registry
+    # $IMAGE_NAME
+    # $IMAGE_TAG
+    # $DEVOPS_SIGNER
+    export DOCKER_CONTENT_TRUST=1
+    echo "Vault instance $VAULT_INSTANCE used to retrieve signing keys"
+    source <(curl -sSL "https://raw.githubusercontent.com/jauninb/jumpstart/master/signatures.sh")
+    # Restore signer pem key
+    VAULT_DATA=$(buildVaultAccessDetailsJSON "$VAULT_INSTANCE" "$IBMCLOUD_TARGET_REGION" "$IBMCLOUD_TARGET_RESOURCE_GROUP")
+    JSON_DATA="$(readData "$REGISTRY_NAMESPACE.keys" "$VAULT_DATA")"
+    signerkey=$(getJSONValue "$DEVOPS_SIGNER" "$JSON_DATA")
+    writeFile "$signerkey"
+    # Retrieve the signer passphrase
+    export DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$(getJSONValue "passphrase" "$signerkey")
+
+    export DCT_DISABLED=false
+    # Pull the image
+    docker pull "$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG"
+    # Sign the image
+    docker trust sign "$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME:$IMAGE_TAG"
+    docker trust inspect --pretty "$REGISTRY_URL/$REGISTRY_NAMESPACE/$IMAGE_NAME"
+}
